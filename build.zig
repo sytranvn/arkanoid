c5b3ec8fa11f0b0951ec29b9d018f5335ab44172
Sy Tran Dung <sytrancsvn@gmail.com>
2024-04-24 13:09:37 +0700

    wip

--- build.zig
+++ build.zig
@@ -1,19 +1,11 @@
 const std = @import("std");
 const Build = std.Build;
+const raylib = @import("raylib");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
+const OptimizeMode = std.builtin.OptimizeMode;
+
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const raylib_dep = b.dependency("raylib", .{
@@ -21,43 +13,66 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const raylib = raylib_dep.artifact("raylib");
-    b.installArtifact(raylib);
+    const raylib_artifact = raylib_dep.artifact("raylib");
+    b.installArtifact(raylib_artifact);
 
     const exe = b.addExecutable(.{
-        .name = "zingzong",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+        .name = "zig-arkanoid",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    exe.linkLibrary(raylib);
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    exe.linkLibrary(raylib_artifact);
     b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+}
+
+// this is the regular build for all native platforms, nothing surprising here
+fn buildNative(b: *Build, target: Build.ResolvedTarget, optimize: OptimizeMode, dep_raylib: *Build.Dependency) !void {
+    const pacman = b.addExecutable(.{
+        .name = "pacman",
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("src/pacman.zig"),
+    });
+    pacman.root_module.addImport("raylib", dep_raylib.module("raylib"));
+    b.installArtifact(pacman);
+    const run = b.addRunArtifact(pacman);
+    b.step("run", "Run pacman").dependOn(&run.step);
+}
+
+// for web builds, the Zig code needs to be built into a library and linked with the Emscripten linker
+fn buildWeb(b: *Build, target: Build.ResolvedTarget, optimize: OptimizeMode, dep_raylib: *Build.Dependency) !void {
+    const pacman = b.addStaticLibrary(.{
+        .name = "pacman",
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("src/pacman.zig"),
+    });
+    pacman.root_module.addImport("raylib", dep_raylib.module("raylib"));
+
+    // create a build step which invokes the Emscripten linker
+    const emsdk = dep_raylib.builder.dependency("emsdk", .{});
+    const link_step = try raylib.emLinkStep(b, .{
+        .lib_main = pacman,
+        .target = target,
+        .optimize = optimize,
+        .emsdk = emsdk,
+        .use_webgl2 = true,
+        .use_emmalloc = true,
+        .use_filesystem = false,
+        .shell_file_path = dep_raylib.path("src/raylib/web/shell.html").getPath(b),
+    });
+    // ...and a special run step to start the web build output via 'emrun'
+    const run = raylib.emRunStep(b, .{ .name = "pacman", .emsdk = emsdk });
+    run.step.dependOn(&link_step.step);
+    b.step("run", "Run pacman").dependOn(&run.step);
 }
